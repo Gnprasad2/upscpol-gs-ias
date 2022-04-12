232ba0c52342c84d381882984a2eaf254001e606
Gnprasad2 <76981773+Gnprasad2@users.noreply.github.com>
2022-04-12 08:23:54 +0530

    update minor carousel not ok

---
+++ images/scripts/index.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+  document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, {
+        fullWidth: true,
+        indicators: true
+      });
+    
+
+  })
