43fb38261854462e515526b2dc3f677fdadf6ee5
Gnprasad2 <76981773+Gnprasad2@users.noreply.github.com>
2022-04-13 07:17:39 +0530

    minor

--- images/scripts/index.js
+++ images/scripts/index.js
@@ -1,46 +1,13 @@
-
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("demo");
-  let captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
-}
+let options = {};
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, options);
+});
 
 
 
-  document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('.carousel');
-    var instances = M.Carousel.init(elems, {
-        fullWidth: true,
-        indicators: true
-      });
-    
 
-  })
+
 
 
   window.onload = (event) => {
@@ -68,6 +35,8 @@ function showSlides(n) {
     ev.preventDefault();
     return false;
   }, false);
+
+
 
 
   
