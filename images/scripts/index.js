2339e1b8eae84a32596c405108abf047e4119f0a
Gnprasad2 <76981773+Gnprasad2@users.noreply.github.com>
2022-04-13 17:47:44 +0530

    minor not ok

--- images/scripts/index.js
+++ images/scripts/index.js
@@ -1,8 +1,49 @@
 let options = {};
+
+$('.sidenav').sidenav();
+
+
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.sidenav');
   var instances = M.Sidenav.init(elems, options);
+
 });
+
+
+// var elements = document.getElementsByClassName("mapLinksList");
+
+
+
+// for (var i = 0; i < elements.length; i++) {
+//     elements[i].addEventListener('click', (event) =>{
+//       console.log(event)
+
+//     }, false);
+// }
+    
+
+
+
+// for mobile sidenav
+
+$(document).ready(function () {
+  // $('.sidenav').sidenav();
+  $('.sidenav').sidenav({
+
+    edge: 'right', // Choose the horizontal origin
+    closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+  });
+});
+
+$('.sidenav li').click(() => {
+  console.log('clicked');
+  // $('.sidenav').sidenav('close');
+})
+
+
+
+
+
 
 
 
@@ -11,6 +52,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
   window.onload = (event) => {
+
   
     // M.AutoInit();
 
